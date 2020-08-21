d42ca1baece6ed5e5e11d6c93df61ef29a3ae5ae
Ems≈Spree <ems.spree@gmail.com>
2020-08-21 07:48:11 +0200

    Update dictionary-openthesaurus-de-tekl.rb

--- Casks/dictionary-openthesaurus-de-tekl.rb
+++ Casks/dictionary-openthesaurus-de-tekl.rb
@@ -7,22 +7,10 @@ cask "dictionary-openthesaurus-de-tekl" do
   desc "Extends Apple’s Dictionary application with a German thesaurus based on data from www.openthesaurus.de"
   homepage "https://github.com/Tekl/openthesaurus-deutsch"
 
-  ## install
   dictionary "OpenThesaurus Deutsch.dictionary"
-  # postflight do
-  #   FileUtils.killall "DictionaryPanel"
-  #   FileUtils.killall "com.apple.DictionaryServiceHelper"
-  #   FileUtils.killall "Dictionary"
-  # end
-
-  ## uninstall
-# uninstall pkgutil "de.tekl.dictionary.openThesaurusDeutsch"
-# uninstall delete  . . .
-# uninstall delete: "#{staged_path}/#{token}"
 
   caveats <<~EOS
-    You may need to activate this plugin
-    in Dictionary’s preferences.
+    You may need to activate this plugin in Dictionary’s preferences.
   EOS
 
 end
