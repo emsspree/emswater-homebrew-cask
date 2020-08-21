3d246fe99ea72672e2ea6e763158d0e9830a120f
Ems≈Spree <ems.spree@gmail.com>
2020-08-21 07:33:17 +0200

    Update dictionary-openthesaurus-de-tekl.rb

--- Casks/dictionary-openthesaurus-de-tekl.rb
+++ Casks/dictionary-openthesaurus-de-tekl.rb
@@ -3,7 +3,7 @@ cask "dictionary-openthesaurus-de-tekl" do
   sha256 :no_check
 
   url "https://github.com/Tekl/openthesaurus-deutsch/releases/latest/download/OpenThesaurus_Deutsch_dictionaryfile.zip"
-  name "OpenThesaurus Deutsch (German)"
+  name "OpenThesaurus Deutsch (German) for Apple’s Dictionary"
   desc "Extends Apple’s Dictionary application with a German thesaurus based on data from www.openthesaurus.de"
   homepage "https://github.com/Tekl/openthesaurus-deutsch"
 
