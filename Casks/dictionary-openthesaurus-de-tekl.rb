6f33ac2b358be9e8fffea384b5c00edc80d44183
EmsSpree <ems.spree@gmail.com>
2022-01-07 04:58:19 +0100

    Update dictionary-openthesaurus-de-tekl.rb

--- Casks/dictionary-openthesaurus-de-tekl.rb
+++ Casks/dictionary-openthesaurus-de-tekl.rb
@@ -1,13 +1,13 @@
-cask "dictionary-openthesaurus-de-tekl" do
+cask 'dictionary-openthesaurus-de-tekl' do
  #version :latest
-  version "2021.12.09"
+  version '2021.12.09'
   sha256 :no_check
 
  #url "https://github.com/Tekl/openthesaurus-deutsch/releases/latest/download/OpenThesaurus_Deutsch_dictionaryfile.zip"
   url "https://github.com/Tekl/openthesaurus-deutsch/releases/download/v#{version}/OpenThesaurus_Deutsch_dictionaryfile.zip"
   name 'OpenThesaurus Deutsch'
   desc 'OpenThesaurus in Deutsch (www.openthesaurus.de) von Tekl für die Lexikon-Anwendung. Extends macOS’ Dictionary application with a German thesaurus based on data from www.openthesaurus.de'
-  homepage "https://github.com/Tekl/openthesaurus-deutsch"
+  homepage 'https://github.com/Tekl/openthesaurus-deutsch'
 
   livecheck do
     url 'https://github.com/Tekl/openthesaurus-deutsch/releases/latest'
