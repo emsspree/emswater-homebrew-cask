4b15ad87c25608a017cbdb5edebc516970468812
EmsSpree <ems.spree@gmail.com>
2022-01-07 03:57:25 +0100

    Update dictionary-openthesaurus-de-tekl.rb

--- Casks/dictionary-openthesaurus-de-tekl.rb
+++ Casks/dictionary-openthesaurus-de-tekl.rb
@@ -4,7 +4,7 @@ cask "dictionary-openthesaurus-de-tekl" do
   sha256 :no_check
 
  #url "https://github.com/Tekl/openthesaurus-deutsch/releases/latest/download/OpenThesaurus_Deutsch_dictionaryfile.zip"
-  url "https://github.com/Tekl/openthesaurus-deutsch/releases/v#{version}/download/OpenThesaurus_Deutsch_dictionaryfile.zip"
+  url "https://github.com/Tekl/openthesaurus-deutsch/releases/download/v#{version}/OpenThesaurus_Deutsch_dictionaryfile.zip"
   name "OpenThesaurus Deutsch"
   desc "OpenThesaurus in Deutsch (www.openthesaurus.de) von Tekl für die Lexikon-Anwendung von macOS. Extends macOS’ Dictionary application with a German thesaurus based on data from www.openthesaurus.de"
   homepage "https://github.com/Tekl/openthesaurus-deutsch"
@@ -21,7 +21,6 @@ cask "dictionary-openthesaurus-de-tekl" do
 
   caveats <<~EOS
     Eventuell muss diese Erweiterung (Referenzquelle) in den Lexikon-Einstellungen aktiviert werden.
-    
     You may need to activate this plugin (reference source) in Dictionary’s preferences.
   EOS
     # Da keine Versionsprüfung stattfindet, wird dieser Cask nicht als »outdated« angezeigt, auch wenn’s so ist. Verwende die Funktion »Auf Update prüfen« in den Lexikon-Einstellungen oder `brew reinstall #{token}` zum Installieren der neuesten Version.
